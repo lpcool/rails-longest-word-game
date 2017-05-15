7e708353e7313ae184857186bbc638f08fe53a90
Lars Pedersen <lpcool@gmail.com>
2017-05-15 17:55:44 +0200

    Longest word

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'game', to: "longest_word#game"
   get 'score', to: "longest_word#score"
 
-  root to: "pages#home"
+  root to: "longest_word#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
